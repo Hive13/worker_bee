6058602492ac9f0f77b56e7891a2e2cce1a2b2e9
vincenpt <vincentp@seapine.com>
2021-05-04 15:26:39 -0400

    Basic bot actions added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
 
+// Load any environment settings from .env files before doing anything else.
+import dotenv from 'dotenv';
+dotenv.config();
 
-function _WriteAMessage() {
-    console.log('Hello.');
+// Slack modules
+import { WebClient } from '@slack/web-api';
+
+const SLACK_TOKEN = process.env.SLACK_TOKEN;
+
+const web = new WebClient(SLACK_TOKEN);
+
+const conversationId = 'C020PABE8HL';
+
+async function _PostMessage(message: string) {
+    const res = await web.chat.postMessage({channel: conversationId, text: message});
+
+    console.log(`Message sent: ${res.ts}`);
 }
 
-_WriteAMessage();
+_PostMessage("Testing out slack client.")
+    .catch((err) => {
+        console.error(`Error: ${err}`);
+    })
