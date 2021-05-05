243076f26c6327502be8202ecff70e448276affc
vincenpt <vincentp@seapine.com>
2021-05-05 19:05:12 -0400

    Added SLACK_SOCKET_TOKEN

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,19 @@ dotenv.config();
 // Slack modules
 import { WebClient } from '@slack/web-api';
 
+/**
+ * Note: I use a local .env file in the project root to define the SLACK_TOKEN. The .env file is on the .gitignore
+ * list so it SHOULDN'T be easily committed to git.
+ */
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
+const SLACK_SOCKET_TOKEN = process.env.SLACK_SOCKET_TOKEN;
+
 const web = new WebClient(SLACK_TOKEN);
-const conversationId = 'CMEGP18F5';
+
+// Conversation Identifiers
+// - C020PABE8HL = #hive13_bot_far
+// - C020MAN5TS6 = #bot_development
+const conversationId = 'C020PABE8HL';
 
 /**
  * Sends a Slack message to the currently configured channel.
